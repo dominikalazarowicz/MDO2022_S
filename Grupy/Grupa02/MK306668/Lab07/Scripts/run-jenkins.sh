47236869eda14b703aa3871fa4600a99549f7095
McMkos <mikos@g.pl>
2022-05-08 17:46:29 +0200

    MK306668 Lab07 - Wszystko zrobione

---
+++ Grupy/Grupa02/MK306668/Lab07/Scripts/run-jenkins.sh
@@ -0,0 +1,35 @@
+echo "Run docker:dind"
+
+docker run \
+  --name jenkins-docker \
+  --rm \
+  --detach \
+  --privileged \
+  --network jenkins \
+  --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  docker:dind \
+  --storage-driver overlay2
+  
+echo "Run Jenkins"
+
+docker run \
+  --name myjenkins \
+  --rm \
+  --detach \
+  --network jenkins \
+  --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client \
+  --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 \
+  --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  myjenkins:v0.2
+  
+  echo "Run ngrok"
+  
+~/ngrok/ngrok http 8080
