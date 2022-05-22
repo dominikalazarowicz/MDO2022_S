d26498cbc0b51f85e51480ee2511ad47d73bf1fc
Jakub Skiba <kubask03@gmail.com>
2022-05-22 14:31:40 +0200

    try

--- ITE/GCL04/JS400219/Lab09/script.sh
+++ ITE/GCL04/JS400219/Lab09/script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p /deploy/jenkins
 wget https://raw.githubusercontent.com/bsponge/gomodifytags/main/jenkins/example -O /deploy/jenkins/example
-wget https://raw.githubusercontent.com/bsponge/gomodifytags/main/jenkins/expected -O /deploy/expected
+wget https://raw.githubusercontent.com/bsponge/gomodifytags/main/jenkins/expected-output -O /deploy/expected
 wget https://raw.githubusercontent.com/bsponge/gomodifytags/main/Dockerfile-test-deploy -O /deploy/Dockerfile-test-deploy
 
 cd /deploy
