56ce5f988f781df80722dfc6123fe4983b6c6a98
Jakub Skiba <kubask03@gmail.com>
2022-05-22 14:01:17 +0200

    update

--- ITE/GCL04/JS400219/Lab09/script.sh
+++ ITE/GCL04/JS400219/Lab09/script.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 mkdir -p /deploy/jenkins
 yum install -y docker
-
 yum install -y wget
 wget https://raw.githubusercontent.com/bsponge/gomodifytags/main/jenkins/example -O /deploy/jenkins/example
 wget https://raw.githubusercontent.com/bsponge/gomodifytags/main/jenkins/expected -O /deploy/expected
 wget https://raw.githubusercontent.com/bsponge/gomodifytags/main/Dockerfile-test-deploy -O /deploy/Dockerfile-test-deploy
 
 cd /deploy
-
-systemctl start docker
 
 docker build -t deploy-test -f Dockerfile-test-deploy --build-arg image=bsponge/deployment-image:latest .
 docker run --name test-deployment deploy-test
